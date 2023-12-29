98d420f1a67e0c0efa8d8961f436036540f0b604
DuskEVE <x987000@gmail.com>
2023-12-29 15:37:37 +0800

    add link to main page banner

--- api/reg.php
+++ api/reg.php
@@ -1,7 +1,8 @@
 <?php
+session_start();
 include_once "./db.php";
 
 $account = ['user'=>$_POST['user'], 'password'=>$_POST['password'], 'email'=>$_POST['email']];
 $User->update($account);
-
+$_SESSION['user'] = $_POST['user'];
 ?>
